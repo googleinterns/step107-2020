0a9bca11ea779981cf2eb9024793fc3715581261
Toba Olokungbemi <tobaolokungbemi@gmail.com>
2020-07-17 21:14:14 +0000

    Create script files for each page and add search results page

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -16,107 +16,14 @@
  * Adds load school info page function to search button.
  */
 function init() {
-  const searchButton = document.getElementById('search-button');
-  searchButton.addEventListener('click', () => loadSchoolInfo());
-}
-
-/**
- * Fetches the data from the College ScoreCard API and populates the college
- *     info html page with the appropriate information.
- */
-function loadSchoolInfo() {
-  const schoolSearch = document.getElementById('school-search').value;
-
-  // Get School Data from API.
-  fetch(getLinkBySchoolName(schoolSearch))
-      .then((response) => response.text())
-      .then((data) => {
-        const parsedData = JSON.parse(data);
-        const schools = parsedData['results'];
-
-        // Get main campus
-        const dataResults = getMainCampus(schools);
-
-        // Basic School Information Variables.
-        const id = getIdFromApiData(dataResults);
-        const ownership = getOwnership(dataResults);
-        const name = getSchoolInfo(dataResults, 'name');
-        const city = getSchoolInfo(dataResults, 'city');
-        const state = getSchoolInfo(dataResults, 'state');
-
-        // Cost Statistics Variables.
-        const inStateTuition = getCostInfo(dataResults, 'in_state');
-        const outOfStateTuition = getCostInfo(dataResults, 'out_of_state');
-
-        // Admissions Statistics Variables.
-        const acceptanceRate = getAcceptanceRate(dataResults);
-        const avgSat = getSatInfo(dataResults, 'average.overall');
-        const avgAct = getActInfo(dataResults, 'midpoint.cumulative');
-
-        // Student Statistic Variables.
-        const numStudents = getNumStudents(dataResults);
-        const numMen = getGender(dataResults, 'men') * numStudents;
-        const numWomen = getGender(dataResults, 'women') * numStudents;
-        const graduationRate4yr = getGraduationRate(dataResults);
-        const numWhiteStudents = getRace(dataResults, 'white') * numStudents;
-        const numAsianStudents = getRace(dataResults, 'asian') * numStudents;
-        const numBlackStudents = getRace(dataResults, 'black') * numStudents;
-        const numHispanicStudents =
-            getRace(dataResults, 'hispanic') * numStudents;
-        const numIndigenousStudents =
-            getRace(dataResults, 'aian') * numStudents;
-        const numMultiracialStudents =
-            getRace(dataResults, 'two_or_more') * numStudents;
-        const numUnreportedRaceStudents = (getRace(dataResults, 'unknown') +
-            getRace(dataResults, 'non_resident_alien')) * numStudents;
-
-        // Set School ID to link to reviews page.
-        const reviewsPageLink = document.getElementById('reviews-button');
-        reviewsPageLink.setAttribute('href', `/comments.html?school-id=${id}`);
-
-        // Name Section.
-        schoolHeader = document.getElementById('school-name');
-        schoolHeader.innerHTML = '';
-        schoolHeader.append(dataResults['school.name']);
-
-        // Description Section.
-        const schoolDesc = document.getElementById('school-desc');
-        schoolDesc.innerHTML = '';
-        schoolDesc.append(`${name} is a ${ownership} University 
-            in ${city}, ${state}`);
-
-        // Cost Section.
-        const costDiv = document.getElementById('cost');
-        costDiv.innerHTML = '';
-        costDiv.append(`In-State Tuition: $${inStateTuition}`);
-        costDiv.append(`Out-of-State Tuition: $${outOfStateTuition}`);
-
-        // Admissions Section.
-        const admissionsDiv = document.getElementById('admissions');
-        admissionsDiv.innerHTML = '';
-        admissionsDiv.append(`Acceptance Rate: ${acceptanceRate}%`);
-        admissionsDiv.append(`Average SAT Score: ${avgSat}`);
-        admissionsDiv.append(`Average ACT Score: ${avgAct}`);
-
-        // Students Section.
-        const studentsDiv = document.getElementById('students');
-        studentsDiv.innerHTML = '';
-        studentsDiv.append(`Population: ${numStudents} Students`);
-        studentsDiv.append(`4 Year Graduation Rate: ${graduationRate4yr}%`);
-
-        // Draw charts.
-        drawRaceChart(numWhiteStudents, numAsianStudents, numBlackStudents,
-            numHispanicStudents, numIndigenousStudents, numMultiracialStudents,
-            numUnreportedRaceStudents);
-        drawGenderChart(numMen, numWomen);
-      });
+  prepSearchButton();
 }
 
 /**
  * Inserts the school name into the college scoreboard API link and
  *     returns the complete link.
  * @param {string} schoolName
- * @return {void}
+ * @return {string}
  */
 function getLinkBySchoolName(schoolName) {
   return ('https://api.data.gov/ed/collegescorecard/v1/schools.json?' +
@@ -139,106 +46,6 @@ function getLinkBySchoolName(schoolName) {
       'school.main_campus,school.institutional_characteristics.level');
 }
 
-// Load the Visualization API and the corechart package.
-google.charts.load('current', {packages: ['corechart']});
-
-// Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawRaceChart);
-google.charts.setOnLoadCallback(drawGenderChart);
-
-/**
- * Creates Donut Pie Chart displaying racial breakdown.
- * @param {number} numWhiteStudents
- * @param {number} numAsianStudents
- * @param {number} numBlackStudents
- * @param {number} numHispanicStudents
- * @param {number} numIndigenousStudents
- * @param {number} numMultiracialStudents
- * @param {number} numUnreportedRaceStudents
- */
-function drawRaceChart(numWhiteStudents, numAsianStudents, numBlackStudents,
-    numHispanicStudents, numIndigenousStudents, numMultiracialStudents,
-    numUnreportedRaceStudents) {
-  const data = google.visualization.arrayToDataTable([
-    ['Race', 'Percentage'],
-    ['White', numWhiteStudents],
-    ['Asian', numAsianStudents],
-    ['Black', numBlackStudents],
-    ['Hispanic', numHispanicStudents],
-    ['Indigenous Ameircan/Alaskan', numIndigenousStudents],
-    ['Two or More Races', numMultiracialStudents],
-    ['Unreported', numUnreportedRaceStudents],
-  ]);
-
-  const options = {
-    title: 'Breakdown by Race',
-    pieHole: 0.4,
-    colors: ['#C6ACA4', '#A4C5C6', '#FFEB99',
-      '#856C8B', '#C6BDA4', '#D4EBD0', '#C68B77'],
-  };
-
-  const chart = new google.visualization.PieChart(document
-      .getElementById('race-piechart'));
-  chart.draw(data, options);
-}
-
-/**
- * Creates Donut Pie Chart displaying gender breakdown.
- * @param {number} numMen
- * @param {number} numWomen
- */
-function drawGenderChart(numMen, numWomen) {
-  const data = google.visualization.arrayToDataTable([
-    ['Gender', 'Percentage'],
-    ['Men', numMen],
-    ['Women', numWomen],
-  ]);
-
-  const options = {
-    title: 'Breakdown by Gender',
-    pieHole: 0.4,
-    colors: ['#D4EBD0', '#A4C5C6'],
-  };
-
-  const chart = new google.visualization.PieChart(document
-      .getElementById('gender-piechart'));
-  chart.draw(data, options);
-}
-
-/**
- * Finds and returns the main campus from a list of main and satellite campuses
- *     of the same name.
- * @param {!Array<!Object>} schools The array of school objects returned from
- *     a fetch query.
- * @return {!Object}
- */
-function getMainCampus(schools) {
-  if (schools.length == 1) {
-    return schools[0];
-  } else {
-    main = {};
-    schools.forEach((campus) => {
-      if (campus['school.main_campus'] == 1) {
-        main = campus;
-      }
-    });
-    return main;
-  }
-}
-
-/**
- * Returns whether the school is public or private.
- * @param {!Object} data
- * @return {string}
- */
-function getOwnership(data) {
-  if (data['school.ownership'] == 1) {
-    return 'public';
-  } else {
-    return 'private';
-  }
-}
-
 /**
  * @param {!Object} data
  * @return {number} School ID.
@@ -254,82 +61,10 @@ function getIdFromApiData(data) {
  *     College Scorecard API.
  * @return {string} School name, city, or state.
  */
-function getSchoolInfo(data, infoName) {
+function getBasicSchoolInfo(data, infoName) {
   return data[`school.${infoName}`];
 }
 
-/**
- * @param {!Object} data
- * @param {string} infoName Specific info to be extracted. Name defined by
- *     College Scorecard API.
- * @return {number} Cost.
- */
-function getCostInfo(data, infoName) {
-  return data[`latest.cost.tuition.${infoName}`];
-}
-
-/**
- * @param {!Object} data
- * @return {number} Acceptance rate as a percentage out of 100.
- */
-function getAcceptanceRate(data) {
-  return data['latest.admissions.admission_rate.overall'] * 100;
-}
-
-/**
- * @param {!Object} data
- * @param {string} infoName Specific SAT info to be extracted. Name defined by
- *     College Scorecard API.
- * @return {number} SAT Section Score.
- */
-function getSatInfo(data, infoName) {
-  return data[`latest.admissions.sat_scores.${infoName}`];
-}
-
-/**
- * @param {!Object} data
- * @param {string} infoName Specific ACT info to be extracted. Name defined by
- *     College Scorecard API.
- * @return {number} ACT Section Score
- */
-function getActInfo(data, infoName) {
-  return data[`latest.admissions.act_scores.${infoName}`];
-}
-
-/**
- * @param {!Object} data
- * @return {number} Total enrolled students.
- */
-function getNumStudents(data) {
-  return data['latest.student.size'];
-}
-
-/**
- * @param {!Object} data
- * @param {string} race Name defined by College Scorecard API.
- * @return {number} Race proportion of students as a decimal out of 1.
- */
-function getRace(data, race) {
-  return data[`latest.student.demographics.race_ethnicity.${race}`];
-}
-
-/**
- * @param {!Object} data
- * @param {string} gender Name defined by College Scorecard API.
- * @return {number} Gender proportion of students as a decimal out of 1.
- */
-function getGender(data, gender) {
-  return data[`latest.student.demographics.${gender}`];
-}
-
-/**
- * @param {!Object} data
- * @return {number} 4 year graduation rate as a percentage out of 100.
- */
-function getGraduationRate(data) {
-  return (data['latest.completion.completion_rate_4yr_100nt'] * 100)
-      .toFixed(1);
-}
 
 /** Adds comments to page. */
 function loadComments() {
@@ -353,7 +88,7 @@ function loadComments() {
  */
 function createCommentElement(name, message, time) {
   const commentElement = document.createElement('p');
-  commentElement.innerText = name + ' posted ' + message + ' on ' + time;
+  commentElement.innerText = `${name} posted ${message} on ${time}`;
   return commentElement;
 }
 
@@ -376,6 +111,49 @@ function getSchoolIdFromUrl() {
   const urlParams = new URLSearchParams(queryString);
   const id = urlParams.get('school-id');
   return id;
+}
+
+/**
+ * Sets the event listener to search school.
+ */
+function prepSearchButton() {
+  const searchButton = document.getElementById('search-button');
+  searchButton.addEventListener('click', () => loadSearchResults());
+}
+
+/** Fetches search results from API and saves in local storage. */
+function loadSearchResults() {
+  const searchValue = document.getElementById('school-search').value;
+
+  fetch(getLinkBySchoolName(searchValue))
+      .then((response) => response.text())
+      .then((data) => {
+        const parsedData = JSON.parse(data);
+        schoolsFetchedDataList = parsedData['results'];
+        localStorage.setItem('schoolsFetchedDataList', 
+            JSON.stringify(schoolsFetchedDataList));
+        let schoolsDataList = [];
+
+        schoolsFetchedDataList.forEach((school) => {
+          const id = getIdFromApiData(school);
+          const name = getBasicSchoolInfo(school,'name');
+          const city = getBasicSchoolInfo(school, 'city');
+          const state = getBasicSchoolInfo(school, 'state');
+
+          const schoolData = {
+              id: id,
+              name: name,
+              city: city,
+              state: state,
+          };
+
+          schoolsDataList.push(schoolData);
+        });
+        console.log(JSON.stringify(schoolsDataList));
+        localStorage.setItem('schoolsDataList', 
+            JSON.stringify(schoolsDataList));
+        location.href = '/search-results.html';
+      });
 }
 
 init();
