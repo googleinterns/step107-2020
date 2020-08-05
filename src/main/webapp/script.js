81dfa9eaa78c80680d959c905d4c6856e78fc63c
Jalen <jjj85@cornell.edu>
2020-08-05 19:08:54 +0000

    Autocomplete

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -17,6 +17,8 @@
  */
 function init() {
   prepSearchButton();
+  setSchoolNames();
+  autocomplete(document.getElementById('school-search'), localStorage.getItem('schoolNames'));
 }
 
 /**
@@ -44,6 +46,35 @@ function getLinkBySchoolName(schoolName) {
       '.unknown,latest.student.demographics.men,latest.student' +
       '.demographics.women,latest.completion.completion_rate_4yr_100nt,' +
       'school.main_campus,school.institutional_characteristics.level');
+}
+
+/**
+ * Inserts the school name into the college scoreboard API link and
+ *     returns the list of school names in the API.
+ * @param {string} schoolName
+ * @return {string}
+ */
+function setSchoolNames(){
+  searchValue = document.getElementById('school-search')
+  fetch(getLinkBySchoolName(searchValue))
+      .then((response) => response.text())
+      .then((data) => {
+        const parsedData = JSON.parse(data);
+        schoolsFetchedDataList = parsedData['results'];
+        const schoolNames = [];
+
+        schoolsFetchedDataList.forEach((school) => {
+          const id = getIdFromApiData(school);
+          const name = getBasicSchoolInfo(school, 'name');
+
+          // Saves school info that willl be displayed in search list.
+          const schoolData = name;
+    
+
+          schoolNames.push(schoolData);
+      });
+    localStorage.setItem('schoolNames', schoolNames);
+    });
 }
 
 /**
@@ -113,6 +144,80 @@ function loadSearchResults() {
             JSON.stringify(schoolIdToInfoDataDictionary));
         location.href = '/search-results.html';
       });
+}
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+    var a, b, i, val = this.value;
+    closeAllLists();
+    if (!val) { return false;}
+    currentFocus = -1;
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    this.parentNode.appendChild(a);
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        b = document.createElement("DIV");
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            b.addEventListener("click", function(e) {
+            inp.value = this.getElementsByTagName("input")[0].value;
+            closeAllLists();
+        });
+        a.appendChild(b);
+      }
+    }
+  });
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        currentFocus++;
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        currentFocus--;
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        e.preventDefault();
+        if (currentFocus > -1) {
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+
+function addActive(x) {
+  /*a function to classify an item as "active":*/
+  if (!x) return false;
+  removeActive(x);
+  if (currentFocus >= x.length) currentFocus = 0;
+  if (currentFocus < 0) currentFocus = (x.length - 1);
+  x[currentFocus].classList.add("autocomplete-active");
+}
+
+function removeActive(x) {
+  for (var i = 0; i < x.length; i++) {
+    x[i].classList.remove("autocomplete-active");
+  }
+}
+
+function closeAllLists(elmnt) {
+  var x = document.getElementsByClassName("autocomplete-items");
+  for (var i = 0; i < x.length; i++) {
+    if (elmnt != x[i] && elmnt != inp) {
+    x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
 }
 
 init();
